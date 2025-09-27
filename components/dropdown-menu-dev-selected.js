3da74e1afabe0ddfb176246b170c690d238d31a6
tom-effernelli <tom.effernelli@gmail.com>
2025-09-27 14:14:20 +0200

    fix: dropdown menu styling and support

--- components/dropdown-menu-dev-selected.js
+++ components/dropdown-menu-dev-selected.js
@@ -2,13 +2,13 @@ import Link from "next/link";
 import Image from "next/image";
 import PropTypes from "prop-types";
 
-const DropdownMenuHomeSelected = ({ className = "" }) => {
+const DropdownMenuDevSelected = ({ className = "", onClose }) => {
   return (
     <div
-      className={`w-full bg-[#000] border-[#fff] border-solid border-b-[2px] box-border max-w-full h-[380px] flex flex-col items-start pt-5 px-5 pb-10 gap-2.5 text-left text-[29px] text-[#fff] ${className}`}
+      className={`w-full bg-[#000] border-[#fff] border-solid border-b-[2px] box-border max-w-full h-[380px] flex flex-col items-start pt-5 pl-[45px] pr-[45px] pb-10 gap-2.5 text-left text-[29px] text-[#fff] ${className}`}
     >
       {/* Icône de fermeture en haut à droite */}
-      <div className="self-stretch flex-1 border-[#fff] border-solid border-b-[2px] overflow-hidden flex items-center justify-end pt-0 px-0 pb-[5px]">
+      <div className="self-stretch flex-1 overflow-hidden flex items-center justify-end pt-0 px-0 pb-0">
         <Image
           className="w-[39px] lg:w-[36px] md:w-[32px] sm:w-[24px] h-[34px] lg:h-[30px] md:h-[28px] sm:h-[20px] cursor-pointer"
           width={39}
@@ -16,6 +16,7 @@ const DropdownMenuHomeSelected = ({ className = "" }) => {
           sizes="100vw"
           alt="Fermer le menu"
           src="/Dropdown-menu-white.svg"
+          onClick={onClose}
         />
       </div>
       <Link
@@ -67,8 +68,9 @@ const DropdownMenuHomeSelected = ({ className = "" }) => {
   );
 };
 
-DropdownMenuHomeSelected.propTypes = {
+DropdownMenuDevSelected.propTypes = {
   className: PropTypes.string,
+  onClose: PropTypes.func,
 };
 
-export default DropdownMenuHomeSelected;
+export default DropdownMenuDevSelected;
--- components/navigation-bar-contact-selected.js
+++ components/navigation-bar-contact-selected.js
@@ -1,12 +1,32 @@
 import Link from "next/link";
 import Image from "next/image";
 import PropTypes from "prop-types";
+import { useState } from "react";
+import DropdownMenuContactSelected from "./dropdown-menu-contact-selected";
 
-const NavigationBarHomeSelected = ({ className = "" }) => {
+const NavigationBarContactSelected = ({ className = "" }) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const closeDropdown = () => {
+    setIsDropdownOpen(false);
+  };
+
   return (
-    <div
-      className={`w-[1524px] bg-[#fff3d9] border-[#000] border-solid border-b-[1px] box-border max-w-full h-[101px] lg:h-[90px] md:h-[80px] sm:h-[60px] overflow-hidden flex items-center justify-between py-0 px-[45px] lg:px-[35px] md:px-[30px] sm:px-[20px] gap-0 text-left text-[29px] lg:text-[26px] md:text-[24px] sm:text-[18px] text-[#000] ${className}`}
-    >
+    <>
+      {isDropdownOpen && (
+        <DropdownMenuContactSelected 
+          className="fixed top-0 left-0 z-50" 
+          onClose={closeDropdown}
+        />
+      )}
+      {!isDropdownOpen && (
+        <div
+          className={`w-[1524px] bg-[#fff3d9] border-[#000] border-solid border-b-[1px] box-border max-w-full h-[101px] lg:h-[90px] md:h-[80px] sm:h-[60px] overflow-hidden flex items-center justify-between py-0 px-[45px] lg:px-[35px] md:px-[30px] sm:px-[20px] gap-0 text-left text-[29px] lg:text-[26px] md:text-[24px] sm:text-[18px] text-[#000] ${className}`}
+        >
       <Link href="/" className="w-[50px] lg:w-[45px] md:w-[40px] sm:w-[30px] relative bg-[#000] box-border h-[50px] lg:h-[45px] md:h-[40px] sm:h-[30px] cursor-pointer" />
       <div className="flex items-center justify-center py-0 pl-[135px] lg:pl-[110px] md:pl-[90px] sm:pl-[60px] pr-[100px] md:pr-[0px] sm:pr-[0px] gap-16 lg:gap-14 md:gap-12 sm:gap-8 lg:flex md:flex md:w-auto md:[align-self:unset] md:items-center md:justify-center md:pl-0 md:pr-[20px] md:box-border sm:flex">
         <div className="flex items-center justify-center md:hidden">
@@ -52,14 +72,17 @@ const NavigationBarHomeSelected = ({ className = "" }) => {
           sizes="100vw"
           alt=""
           src="/Dropdown-menu.svg"
+          onClick={toggleDropdown}
         />
       </div>
     </div>
+      )}
+    </>
   );
 };
 
-NavigationBarHomeSelected.propTypes = {
+NavigationBarContactSelected.propTypes = {
   className: PropTypes.string,
 };
 
-export default NavigationBarHomeSelected;
+export default NavigationBarContactSelected;
