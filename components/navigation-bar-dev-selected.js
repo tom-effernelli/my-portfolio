99da81559ba50306aaf544dacffc6300909ffdbb
tom-effernelli <tom.effernelli@gmail.com>
2025-10-24 14:32:53 +0200

    feat: navigation bar adapation for xs breakpoint

--- components/navigation-bar-dev-selected.js
+++ components/navigation-bar-dev-selected.js
@@ -29,10 +29,10 @@ const NavigationBarDevSelected = ({ className = "" }) => {
       )}
       {!isDropdownOpen && (
         <div
-          className={`navigation-bar-dev w-[1524px] bg-[#000] border-[#fff] border-solid border-b-[1px] box-border max-w-full h-[101px] lg:h-[90px] md:h-[80px] sm:h-[60px] overflow-hidden flex items-center justify-between py-0 px-[45px] lg:px-[35px] md:px-[30px] sm:px-[20px] gap-0 text-left text-[29px] lg:text-[26px] md:text-[24px] sm:text-[18px] text-[#fff] ${className}`}
+          className={`navigation-bar-dev w-[1524px] bg-[#000] border-[#fff] border-solid border-b-[1px] box-border max-w-full h-[101px] lg:h-[90px] md:h-[80px] sm:h-[60px] xs:h-[50px] overflow-hidden flex items-center justify-between py-0 px-[45px] lg:px-[35px] md:px-[30px] sm:px-[20px] xs:px-[15px] gap-0 text-left text-[29px] lg:text-[26px] md:text-[24px] sm:text-[18px] xs:text-[16px] text-[#fff] ${className}`}
         >
-      <Link href="/" className="w-[50px] lg:w-[45px] md:w-[40px] sm:w-[30px] relative bg-[#fff] box-border h-[50px] lg:h-[45px] md:h-[40px] sm:h-[30px] cursor-pointer" />
-      <div className="flex items-center justify-center py-0 pl-[135px] lg:pl-[110px] md:pl-[90px] sm:pl-[60px] pr-[20px] gap-16 lg:gap-14 md:gap-12 sm:gap-8 lg:flex md:flex md:w-auto md:[align-self:unset] md:items-center md:justify-center md:pl-0 md:box-border sm:flex">
+      <Link href="/" className="w-[50px] lg:w-[45px] md:w-[40px] sm:w-[30px] xs:w-[25px] relative bg-[#fff] box-border h-[50px] lg:h-[45px] md:h-[40px] sm:h-[30px] xs:h-[25px] cursor-pointer" />
+      <div className="flex items-center justify-center py-0 pl-[135px] lg:pl-[110px] md:pl-[90px] sm:pl-[60px] xs:pl-[40px] gap-16 lg:gap-14 md:gap-12 sm:gap-8 xs:gap-6 lg:flex md:flex md:w-auto md:[align-self:unset] md:items-center md:justify-center md:pl-0 md:box-border sm:flex">
         <div className="flex items-center justify-center md:hidden xs:hidden sm:hidden">
           <Link
             className="cursor-pointer [text-decoration:none] relative leading-[121.1%] font-medium text-[inherit]"
@@ -70,7 +70,7 @@ const NavigationBarDevSelected = ({ className = "" }) => {
           </Link>
         </div>
         <Image
-          className="w-[39px] lg:w-[36px] md:w-[32px] sm:w-[24px] relative h-[34px] lg:h-[30px] md:h-[28px] sm:h-[20px] hidden md:block md:cursor-pointer xs:block sm:block sm:cursor-pointer select-none"
+          className="w-[39px] lg:w-[36px] md:w-[32px] sm:w-[24px] xs:w-[20px] relative h-[34px] lg:h-[30px] md:h-[28px] sm:h-[20px] xs:h-[18px] hidden md:block md:cursor-pointer xs:block sm:block sm:cursor-pointer select-none"
           width={39}
           height={34}
           sizes="100vw"
